61bddb2c29c319f0de1c11d6d0066bfde868084f
xcy <1120361817@qq.com>
2016-06-02 15:37:39 +0800

    xxx

---
+++ MySimpleMvc/SimpleMvc_Lib/Routing/UrlRoutingModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace SimpleMvc_Lib.Routing
+{
+    public class UrlRoutingModule : IHttpModule
+    {
+
+        public void Init(HttpApplication context)
+        {
+            context.PostResolveRequestCache += Application_PostResolveRequestCache;
+        }
+
+        public void Application_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            var application = sender as HttpApplication;
+            var context = application.Context;
+
+            string path = context.Request.AppRelativeCurrentExecutionFilePath.Substring(2);
+            IDictionary<string, string> route = new Dictionary<string, string>();
+            //route = 
+        }
+
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+    }
+}
